d1872c80845318b280e5e2b6913eb90089727b86
miikka.s.roine <miikka.s.roine@accenture.com>
2016-11-20 00:08:37 +0200

    commits from Miikka

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to My test Application for REACT</h2>
+          <h2>Welcome to My test Application for React</h2>
         </div>
         <p className="App-intro">
           My test APP Demo
