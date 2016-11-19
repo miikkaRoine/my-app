b75c0e4783c8b2d7d41b27afe8c2a3b0f7d0d978
miikka.s.roine <miikka.s.roine@accenture.com>
2016-11-19 23:05:43 +0200

    content update 6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to My test App</h2>
         </div>
         <p className="App-intro">
           My test APP demonstration
