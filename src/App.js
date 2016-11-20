c4b8a7c26eaa918082f5a3ef5af656b96e3867b2
miikka.s.roine <miikka.s.roine@accenture.com>
2016-11-20 12:21:52 +0200

    CI Test commit to Master

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to My test Application for React</h2>
+          <h2>Hurrayh!</h2>
         </div>
         <p className="App-intro">
-          My test APP Demo
+          From local -> GitHub -> Travis -> Heroku Web Dyno
         </p>
       </div>
     );
